0dc5b9e2e0178967bb63e494c6eb0eedf89a80da
ZhuLei <zhulei@xs1h.com>
2018-05-13 02:00:07 +0800

    .

---
+++ src/app.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import A from './components/a';
+import B from './components/b';
+import C from './components/c';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      num: 1
+    };
+  }
+
+  componentDidMount() {
+    setInterval(_ => {
+      this.setState({
+        num: (this.state.num + 1) % 24
+      });
+    }, 10000);
+  }
+
+  render() {
+    const { num } = this.state;
+    console.log('I render', +new Date);
+
+    return (
+      <div>
+        {num % 2 && <A /> || null}
+        {num % 3 && <A /> || null}
+        {num % 6 && <A /> || null}
+        {num % 8 && <B /> || null}
+        {num % 12 && <B /> || null}
+        {num % 24 && <C /> || null}
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import A from './components/a';
-import B from './components/b';
-import C from './components/c';
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      num: 1
-    };
-  }
-
-  componentDidMount() {
-    setInterval(_ => {
-      this.setState({
-        num: (this.state.num + 1) % 24
-      });
-    }, 10000);
-  }
-
-  render() {
-    const { num } = this.state;
-    console.log('I render', +new Date);
-
-    return (
-      <div>
-        {num % 2 && <A /> || null}
-        {num % 3 && <A /> || null}
-        {num % 6 && <A /> || null}
-        {num % 8 && <B /> || null}
-        {num % 12 && <B /> || null}
-        {num % 24 && <C /> || null}
-      </div>
-    )
-  }
-}
+import App from './app';
 
 ReactDOM.render(
   <App />,
